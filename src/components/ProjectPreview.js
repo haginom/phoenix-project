bfcf5a8af7c4f72a68cfe0491068571732094097
Saki Reid <saki.reid@gmail.com>
2023-10-12 09:27:40 +0200

    adjusting logo on preview side and research chevron

--- src/components/ProjectPreview.js
+++ src/components/ProjectPreview.js
@@ -34,7 +34,7 @@ function ProjectPreview({
                 className="previewPosterImg absolute"
                 asset={props.logo?.asset}
                 style={{
-                  width: "80%",
+                  width: "65%",
                   height: "100%",
                   objectFit: "contain",
                   left: "50%",
